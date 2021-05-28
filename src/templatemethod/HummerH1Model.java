dcda91a5bdb709783294fbec23d219d201ebaa3f
10471 <1047106689@qq.com>
2021-05-28 11:10:39 +0800

    设计模式之禅-模板方法模式

---
+++ src/templatemethod/HummerH1Model.java
@@ -0,0 +1,34 @@
+package templatemethod;
+
+public class HummerH1Model extends HummerModel {
+    private boolean alarmFlag = true;
+
+    @Override
+    protected void start() {
+        System.out.println("悍马H1发动...");
+    }
+
+    @Override
+    protected void stop() {
+        System.out.println("悍马H1停车...");
+    }
+
+    @Override
+    protected void alarm() {
+        System.out.println("悍马H1鸣笛...");
+    }
+
+    @Override
+    protected void engineBoom() {
+        System.out.println("悍马H1引擎声音是这样在...");
+    }
+
+    @Override
+    protected boolean isAlarm() {
+        return this.alarmFlag;
+    }
+
+    public void setAlarm(boolean isAlarm) {
+        this.alarmFlag = isAlarm;
+    }
+}
